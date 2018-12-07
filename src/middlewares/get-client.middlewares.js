4732d5708ee669e1141c274f495785e4550b8739
zhangucan <347947478@qq.com>
2018-12-07 16:48:33 +0800

    WIP

--- src/middlewares/get-client.middlewares.js
+++ src/middlewares/get-client.middlewares.js
@@ -2,9 +2,12 @@ const DotEnv = require("dotenv");
 const Clients = require("../../.clients.js");
 
 const getClient = (req, res, next) => {
+  const env = DotEnv.config();
+  console.log(env);
   const client = req.header("client")
     ? req.header("client")
     : process.env.SERVER_DEFAULT_CLIENT;
+  console.log(Clients);
   req.$client = Clients[client] ? Clients[client] : {};
 
   if (!req.$client || !Object.keys(req.$client).length) {
